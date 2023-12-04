924ae635f48e3becc95322458fbfec5cc49755d9
Mateen <mateenqazi125@gmail.com>
2023-12-04 16:05:52 +0500

    terraform block

--- 03-Terraform-Fundamental-Blocks/03-01-Terraform-Block/terraform-manifests/terraform-block.tf
+++ 03-Terraform-Fundamental-Blocks/03-01-Terraform-Block/terraform-manifests/terraform-block.tf
@@ -1,16 +1,17 @@
 # Terraform Block
 terraform {
-  required_version = ">= 1.4.1"
+  required_version = "~> 1.6.5"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.0"
+      version = "~> 5.0"
     }
     random = {
       source = "hashicorp/random"
-      version = "3.4.3"
-    }    
+      version = "3.5.1"
+    }
   }
+
 }
 
 /*
