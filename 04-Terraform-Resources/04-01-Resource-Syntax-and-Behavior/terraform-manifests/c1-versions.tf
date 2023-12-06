8c1a5b7cf649dd3d815095eea4acd12ebcc6aa50
Mateen <mateenqazi125@gmail.com>
2023-12-06 17:51:31 +0500

    state in resources

--- 04-Terraform-Resources/04-01-Resource-Syntax-and-Behavior/terraform-manifests/c1-versions.tf
+++ 04-Terraform-Resources/04-01-Resource-Syntax-and-Behavior/terraform-manifests/c1-versions.tf
@@ -1,15 +1,15 @@
 # Terraform Block
 terraform {
-  required_version = ">= 1.4"
+  required_version = ">= 1.6.5"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = ">= 5.0"
     }
   }
 }
 
 # Provider Block
 provider "aws" {
-  region  = "us-east-1"
+  region = "us-east-1"
 }
