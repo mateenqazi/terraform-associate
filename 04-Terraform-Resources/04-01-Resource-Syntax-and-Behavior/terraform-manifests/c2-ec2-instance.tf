3bfd3622eaea0155b51a09a5db05571b23bf7073
Mateen <mateenqazi125@gmail.com>
2023-12-06 17:56:29 +0500

    revert

--- 04-Terraform-Resources/04-01-Resource-Syntax-and-Behavior/terraform-manifests/c2-ec2-instance.tf
+++ 04-Terraform-Resources/04-01-Resource-Syntax-and-Behavior/terraform-manifests/c2-ec2-instance.tf
@@ -5,9 +5,8 @@
 resource "aws_instance" "my-ec2-terraform" {
   ami               = "ami-0230bd60aa48260c6"
   instance_type     = "t2.micro"
-  availability_zone = "us-east-1b"
+  availability_zone = "us-east-1a"
   tags = {
     "Name" = "web"
-    "tag1" = "update-test-1"
   }
 }
