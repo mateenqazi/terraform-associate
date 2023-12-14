f8437913f2689a1805bf67e4dfde5765ad7e352e
Mateen <mateenqazi125@gmail.com>
2023-12-14 15:27:39 +0500

    for_each practice in resources

--- 04-Terraform-Resources/04-04-Meta-Argument-for_each/v2-for_each-toset/c2-iamuser.tf
+++ 04-Terraform-Resources/04-04-Meta-Argument-for_each/v2-for_each-toset/c2-iamuser.tf
@@ -2,6 +2,6 @@
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_user
 
 resource "aws_iam_user" "myuser" {
-  for_each = toset(["TJack", "TJames", "TMadhu", "TDave"])
-  name     = each.key
+  for_each = toset(["mateen", "qazi", "mq"])
+  name     = each.value
 }
